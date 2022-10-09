6f7ebb1b5a8f4d2e979f6be727bd80fe3a4aacf8
Sup3r-Us3r <maydersonmello@gmail.com>
2022-10-08 21:31:34 -0300

    create theme setup with stitches

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@sup3r-us3r/tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
