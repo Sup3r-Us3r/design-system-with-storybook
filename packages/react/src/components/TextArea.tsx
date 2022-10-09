747dc8d62438457dcd86ae33447aaafcaec22a7f
Sup3r-Us3r <maydersonmello@gmail.com>
2022-10-08 21:30:50 -0300

    create text area component to design system

---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,39 @@
+import { ComponentProps } from 'react'
+
+import { styled } from '../styles'
+
+const TextArea = styled('textarea', {
+  display: 'flex',
+  alignItems: 'baseline',
+  backgroundColor: '$gray900',
+  padding: '$3 $4',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  border: '2px solid $gray900',
+  fontFamily: '$default',
+  fontSize: '$sm',
+  color: '$white',
+  fontWeight: '$regular',
+  resize: 'vertical',
+  minHeight: 80,
+
+  '&:focus': {
+    outline: 0,
+    borderColor: '$ignite300',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
+
+TextArea.displayName = 'TextArea'
+
+type TextAreaProps = ComponentProps<typeof TextArea>
+
+export { type TextAreaProps, TextArea }
